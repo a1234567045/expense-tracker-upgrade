3a28b29021237ccbcd87a0b9aa38338672deac9f
wei <a1234567045@yahoo.com.tw>
2021-05-27 14:59:48 +0800

    feat: add hbs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -13,9 +13,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 
 
