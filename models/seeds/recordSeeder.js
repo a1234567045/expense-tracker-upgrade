25306ab59837dca35a40f90aaf067e7ae56d2149
wei <a1234567045@yahoo.com.tw>
2021-06-06 14:44:11 +0800

    feat:modify

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,6 +1,6 @@
 require('../../config/mongoose')
 // 載入資料
-const Record = require('../record') 
+const Record = require('../record')
 const recordList = require('./record.json')
 const db = require('../../config/mongoose')
 //連線成功
@@ -13,5 +13,5 @@ db.once('open', () => {
       amount: recordList[i].amount
     })
   }
-  console.log('done')
+  console.log('Seeder create')
 })
