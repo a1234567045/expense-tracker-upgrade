d129a3c33ad44a89f0a583dafd3e9adae37a9e4c
wei <a1234567045@yahoo.com.tw>
2021-06-26 00:44:33 +0800

    modify

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
-const usePassport = require('./config/passport')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const flash = require('connect-flash')
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+const session = require('express-session')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+require('./config/mongoose')
+
+const usePassport = require('./config/passport')
+const routes = require('./routes')
+const PORT = process.env.PORT
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+usePassport(app)
+app.use(flash())
+
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
@@ -26,44 +51,10 @@ const hbs = exphbs.create({
   }
 })
 
-const PORT = process.env.PORT || 3000
-require('./config/mongoose')
-
-
-// routes
-const routes = require('./routes')
-const app = express()
-
-
-
-
-
-
 app.engine('hbs', hbs.engine)
+
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
-
-usePassport(app)
-app.use(flash())
-
-
-app.use((req, res, next) => {
-  // 你可以在這裡 console.log(req.user) 等資訊來觀察
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
-  next()
-})
-
 app.use(routes)
 
 app.listen(PORT, () => {
